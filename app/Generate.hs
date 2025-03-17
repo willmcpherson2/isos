3d657e24610009f63f2e25d4ec8abfb00507dd05
William McPherson <willmcpherson2@gmail.com>
2025-03-17 23:34:42 +1100

    Call codegen from Haskell

--- app/Generate.hs
+++ app/Generate.hs
@@ -1,6 +1,7 @@
 module Generate (generate) where
 
 import Control.Monad (when)
+import Data.Vector.Storable (thaw)
 import Foreign (Ptr)
 import Language.C.Inline.Cpp qualified as C
 import Lir
@@ -8,18 +9,61 @@ import System.Exit (ExitCode (..), exitWith)
 
 data State
 
-C.context $ C.cppCtx <> C.cppTypePairs [("State", [t|State|])]
+C.context $ C.baseCtx <> C.vecCtx <> C.cppCtx <> C.cppTypePairs [("State", [t|State|])]
 
 C.include "state.h"
 
 check :: Ptr State -> IO ()
 check state = do
-  code <- fromEnum <$> [C.exp| int { $(State* state)->error } |]
+  code <- fromEnum <$> [C.exp| int { $(State *state)->error } |]
   when (code > 0) $ do
-    [C.exp| void { $(State* state)->printError() } |]
+    [C.exp| void { $(State *state)->printError() } |]
     exitWith $ ExitFailure code
 
 generate :: Prog -> IO ()
 generate prog = do
-  state <- [C.exp| State* { new State() } |]
+  state <- [C.exp| State * { new State() } |]
   check state
+
+  mapM_ (genData state) prog.datas
+  mapM_ (genFuns state) prog.funs
+  genMain state prog.entry
+
+  [C.exp| void { $(State *state)->print() } |]
+  [C.exp| void { $(State *state)->write() } |]
+  check state
+
+genData :: Ptr State -> Data -> IO ()
+genData state Data {symbol, arity} =
+  [C.exp| void { $(State *state)->data($(int symbol), $(int arity)) } |]
+
+genFuns :: Ptr State -> Fun -> IO ()
+genFuns state Fun {symbol, arity, block} = do
+  [C.exp| void { $(State *state)->fun($(int symbol), $(int arity)) } |]
+  genBlock state block
+
+genMain :: Ptr State -> Block -> IO ()
+genMain state block = do
+  [C.exp| void { $(State *state)->main() } |]
+  genBlock state block
+
+genBlock :: Ptr State -> Block -> IO ()
+genBlock = mapM_ . genOp
+
+genOp :: Ptr State -> Op -> IO ()
+genOp state = \case
+  LoadData {name, symbol} -> [C.exp| void { $(State *state)->loadData($(int name), $(int symbol)) } |]
+  LoadArg {name, var, arg} -> [C.exp| void { $(State *state)->loadArg($(int name), $(int var), $(int arg)) } |]
+  Copy {name, var} -> error "todo"
+  Call {name, var} -> [C.exp| void { $(State *state)->call($(int name), $(int var)) } |]
+  ReturnTerm {var} -> [C.exp| void { $(State *state)->returnTerm($(int var)) } |]
+  ReturnSymbol {var} -> [C.exp| void { $(State *state)->returnSymbol($(int var)) } |]
+  FreeArgs {var} -> [C.exp| void { $(State *state)->freeArgs($(int var)) } |]
+  FreeTerm {var} -> error "todo"
+  AppNew {name, var, args} -> do
+    mArgs <- thaw args
+    [C.exp| void { $(State *state)->appNew($(int name), $(int var), $vec-len:mArgs, $vec-ptr:(int *mArgs)) } |]
+  AppFrom {name, old, var, args} -> error "todo"
+  PartialNew {name, var, args} -> error "todo"
+  PartialFrom {name, old, var, args} -> error "todo"
+  Match {var, arms} -> error "todo"
--- app/Lir.hs
+++ app/Lir.hs
@@ -1,27 +1,30 @@
 -- Low-level Intermediate Representation
 module Lir (Prog (..), Data (..), Fun (..), Block, Op (..), Arm (..)) where
 
+import Language.C.Inline.Cpp (CInt)
+import Data.Vector.Storable (Vector)
+
 data Prog = Prog {datas :: [Data], funs :: [Fun], entry :: Block}
 
-data Data = Data {symbol :: Int, arity :: Int}
+data Data = Data {symbol :: CInt, arity :: CInt}
 
-data Fun = Fun {symbol :: Int, arity :: Int, code :: Block}
+data Fun = Fun {symbol :: CInt, arity :: CInt, block :: Block}
 
 type Block = [Op]
 
 data Op
-  = LoadData {name :: Int, symbol :: Int}
-  | LoadArg {name :: Int, var :: Int, arg :: Int}
-  | Copy {name :: Int, var :: Int}
-  | Call {name :: Int, var :: Int}
-  | ReturnTerm {var :: Int}
-  | ReturnSymbol {var :: Int}
-  | FreeArgs {var :: Int}
-  | FreeTerm {var :: Int}
-  | AppNew {name :: Int, var :: Int, args :: [Int]}
-  | AppFrom {name :: Int, old :: Int, var :: Int, args :: [Int]}
-  | PartialNew {name :: Int, var :: Int, args :: [Int]}
-  | PartialFrom {name :: Int, old :: Int, var :: Int, args :: [Int]}
-  | Match {var :: Int, arms :: [Arm]}
+  = LoadData {name :: CInt, symbol :: CInt}
+  | LoadArg {name :: CInt, var :: CInt, arg :: CInt}
+  | Copy {name :: CInt, var :: CInt}
+  | Call {name :: CInt, var :: CInt}
+  | ReturnTerm {var :: CInt}
+  | ReturnSymbol {var :: CInt}
+  | FreeArgs {var :: CInt}
+  | FreeTerm {var :: CInt}
+  | AppNew {name :: CInt, var :: CInt, args :: Vector CInt}
+  | AppFrom {name :: CInt, old :: CInt, var :: CInt, args :: Vector CInt}
+  | PartialNew {name :: CInt, var :: CInt, args :: Vector CInt}
+  | PartialFrom {name :: CInt, old :: CInt, var :: CInt, args :: Vector CInt}
+  | Match {var :: CInt, arms :: [Arm]}
 
-data Arm = Arm {symbol :: Int, code :: Block}
+data Arm = Arm {symbol :: CInt, block :: Block}
