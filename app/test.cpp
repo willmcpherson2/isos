39720b120b9a0e9b52adc4e33d5393c762c324e9
William McPherson <willmcpherson2@gmail.com>
2025-03-22 22:14:32 +1100

    Reorganise symbols in tests

--- app/test.cpp
+++ app/test.cpp
@@ -51,13 +51,13 @@ void testIdentity() {
   State state{};
   CHECK(state);
 
-  // False
-  state.data(0, 0); // 0 = False
   // True
   state.data(1, 0); // 1 = True
+  // False
+  state.data(2, 0); // 2 = False
 
   // id x = x
-  state.fun(2, 1);        // 2 = id
+  state.fun(3, 1);        // 3 = id
   state.loadArg(1, 0, 0); // 1 = 0.args[0]
   state.freeArgs(0);      // free 0.args
   state.call(2, 1);       // 2 = 1.fun(1)
@@ -65,7 +65,7 @@ void testIdentity() {
 
   // main = id True
   state.main();                // main
-  state.loadData(1, 2);        // 1 = id
+  state.loadData(1, 3);        // 1 = id
   state.loadData(2, 1);        // 2 = True
   int args[] = {2};            //
   state.appNew(3, 1, 1, args); // 3 = app_new(1, 2)
